17b20d48de0e4835a6f85cf0d8b7ba74418956a7
Andrew Burgess <andrew@deceptacle.com>
2010-02-12 12:35:30 -0500

    Draws some stuff, saves the image on exit

---
+++ MouseTracker/MouseTracker/MainForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace MouseTracker
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.updateTimer = new System.Windows.Forms.Timer(this.components);
+			this.drawTimer = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
+			// 
+			// updateTimer
+			// 
+			this.updateTimer.Interval = 50;
+			this.updateTimer.Tick += new System.EventHandler(this.updateTimer_Tick);
+			// 
+			// drawTimer
+			// 
+			this.drawTimer.Interval = 1000;
+			this.drawTimer.Tick += new System.EventHandler(this.drawTimer_Tick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(491, 276);
+			this.Name = "MainForm";
+			this.Text = "Mouse Tracker";
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Timer updateTimer;
+		private System.Windows.Forms.Timer drawTimer;
+	}
+}
+
---
+++ MouseTracker/MouseTracker/MainForm.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace MouseTracker
+{
+	public partial class MainForm : Form
+	{
+		private Bitmap bitmap;
+		private Point previousPoint;
+		private float counter;
+
+		public MainForm()
+		{
+			InitializeComponent();
+			previousPoint = new Point(-1, -1);
+		}
+
+		[DllImport("user32.dll")]
+		private static extern bool GetCursorPos(ref Point lpPoint);
+
+		private void updateTimer_Tick(object sender, System.EventArgs e)
+		{
+			var point = new Point();
+
+			GetCursorPos(ref point);
+
+			point = new Point(point.X / 2, point.Y / 2);
+
+			Text = "Mouse Tracker: (" + point.X + ", " + point.Y + ")";
+
+			if (previousPoint.X == -1)
+			{
+				previousPoint = point;
+				return;
+			}
+
+			var graphics = Graphics.FromImage(bitmap);
+
+			if (previousPoint == point)
+			{
+				counter += 0.01f;
+
+				graphics.FillEllipse(new SolidBrush(Color.Black), point.X - counter / 2, point.Y - counter / 2, counter, counter);
+			}
+			else
+			{
+				counter = 0;
+				var distance = (float)Math.Sqrt(Math.Pow(point.X - previousPoint.X, 2) + Math.Pow(point.Y - previousPoint.Y, 2));
+				var size = 15 / distance;
+				graphics.DrawLine(new Pen(Color.Black, size), previousPoint, point);
+			}
+			graphics.Dispose();
+
+			previousPoint = point;
+		}
+
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			var rect = new Rectangle();
+
+			foreach (var screen in Screen.AllScreens)
+			{
+				rect.Width += screen.Bounds.Width;
+				rect.Height = Math.Max(rect.Height, screen.Bounds.Height);
+			}
+
+			Size = new Size(rect.Width / 4, rect.Height / 4);
+
+			bitmap = new Bitmap(rect.Width / 2, rect.Height / 2, PixelFormat.Format24bppRgb);
+			var graphics = Graphics.FromImage(bitmap);
+			graphics.Clear(Color.White);
+			graphics.Dispose();
+
+			updateTimer.Enabled = true;
+			updateTimer.Start();
+
+			drawTimer.Enabled = true;
+			drawTimer.Start();
+		}
+
+		private void MainForm_Paint(object sender, PaintEventArgs e)
+		{
+			var graphics = e.Graphics;
+
+			graphics.DrawImage(bitmap, 0, 0, Width, Height);
+		}
+
+		private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			bitmap.Save("image.jpg");
+		}
+
+		private void drawTimer_Tick(object sender, System.EventArgs e)
+		{
+			Invalidate();
+		}
+	}
+}
